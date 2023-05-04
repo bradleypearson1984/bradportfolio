c2c924a05cbc2c567f0dccdbc7e1c2f7c1465b8b
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-05-03 21:16:17 -0400

    adds photo and some styling

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
 import Image from 'next/image';
-
+import styles from '@/styles/Header.module.css';
 
 function Header() {
     return (
-        <header>
-            //Add image tag here 
-            <h1>Hi I'm Brad Pearson, a human person </h1>            
+        <header className={`${styles.header} flex-ctr-centered`} >
+            <div className='flex-ctr-centered'>
+
+                <Image src="/breezybrad.jpg" height={200} width={200}/>
+                <h1>Hi I'm Brad Pearson, a human person </h1>            
+            </div>
         </header>
     )
 }

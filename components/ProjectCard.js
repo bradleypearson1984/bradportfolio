e52eb14b3fc77535f52edf3ea10fe9c82b0903ec
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-05-03 22:02:00 -0400

    adds projects

---
+++ components/ProjectCard.js
@@ -0,0 +1,23 @@
+// import Image from 'next/image';
+
+function ProjectCard(props) {
+    return (
+        <div>
+            <img 
+            src={props.src} 
+            // height={200} 
+            // width={200} 
+            alt={props.title}
+            />
+            <h3>{props.title}</h3>
+            <style jsx>{`
+                h3 {
+                    text-align:center;
+                }
+            `}</style>
+        </div>
+            );
+}
+
+export default ProjectCard;
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import ProjectCard from '@/components/ProjectCard';
+import projectData from '@/public/project-data';
+
+console.log(projectData)
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +18,40 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={inter.className}>
-        <h1>Home Page</h1>
+      <main className={`${inter.className} ${styles.homeMain}`}>
+      <section className={styles.pageSection} >
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Mollitia fugiat quidem eveniet, praesentium facere nihil iusto perspiciatis distinctio. 
+          Itaque culpa et sit sapiente voluptate aperiam quasi beatae, adipisci hic debitis.
+          </p>
+      </section >
+          <section className={styles.pageSection} >
+            <h2>Skills</h2>
+          </section  >
+          <section className={styles.pageSection} >
+            <h2>Projects</h2>
+            <div className={styles.projectsWrapper} >
+
+            {
+              projectData.map((project, index) => {
+                return (
+
+                  <ProjectCard 
+                    src={project.src}
+                    title={project.title}
+                    key={index}
+                  />
+                  )
+                })
+              }
+              </div>
+              </section>
+          <section className={styles.pageSection} >
+            <h2>Contact</h2>
+          </section>
       </main>
     </>
-  )
+  );
 }

f75ede6bb2cf54c2a15755e0dfb1a0eff8477e04
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-05-09 22:06:56 -0400

    update photo

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header className={`${styles.header} flex-ctr-centered`} >
             <div className='flex-ctr-centered'>
 
-                <Image src="/breezybrad.jpg" height={200} width={200}/>
+                <Image src="/replicant2.jpg" height={200} width={200}/>
                 <h1>Hi I'm Brad Pearson, a human person </h1>            
             </div>
         </header>

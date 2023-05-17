becc0fe14c960b3a769dd424752bbfe81fede3be
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-05-16 20:54:10 -0400

    again fixing

--- pages/petpurpose.jsx
+++ pages/petpurpose.jsx
@@ -21,9 +21,9 @@ const petpurpose = () => {
                     <p>This app was built in React and mongoDB, it connects to the Petfinder API to allow 
                         users to search for dogs, cats and rabbits in shelters in their area. 
                     </p>
-                    <Link to='https://pages.git.generalassemb.ly/brad-pearson-forever/Project-1/' >
+              
                     <button  className='px-8 py-2 mt-4 mr-8' >Demo</button>
-                    </Link>
+                
                     <Link>
                     <button className='px-8 py-2 mt-4' >Code</button> 
                     </Link>
